a3df9601decb6a83268e7085071169067c538e6e
PeppermintNDaisies <julianajaimej@gmail.com>
2020-04-07 11:55:47 -0500

    Working on Singup issues

--- components/common/provider-auth-button.tsx
+++ components/common/provider-auth-button.tsx
@@ -1,17 +1,28 @@
 import { capitalCase } from 'change-case'
 import styles from './provider-auth-button.module.css'
+import logo from '../../assets/login/btn_google_signin_light_normal_web@2x.png'
 
 const ProviderAuthButton = ({ provider, onClick, icon }) => (
-  <button
-    className={`${styles.button} ${styles[provider]}`}
-    onClick={onClick}>
-    <div className={styles['btn-icon']}>
-      <img src={icon} alt={provider} />
-    </div>
-    <span className={styles['btn-text']}>
-      {`Sign in with ${capitalCase(provider)}`}
-    </span>
-  </button>
+  <>
+    {provider === 'google' ?
+      <button
+        onClick={onClick}>
+        <img src={logo} className={styles.google} />
+      </button>
+      :
+      <button
+        className={`${styles.button} ${styles[provider]}`}
+        onClick={onClick}>
+
+        <div className={styles['btn-icon']}>
+          <img src={icon} alt={provider} />
+        </div>
+        <span className={styles['btn-text']}>
+          {`Sign in with ${capitalCase(provider)}`}
+        </span>
+      </button>
+    }
+  </>
 )
 
 export default ProviderAuthButton
