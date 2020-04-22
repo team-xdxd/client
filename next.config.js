9f4a0340f9198f93619ada59d77cd378f20b2674
Santiago <santibelca@gmail.com>
2020-04-21 20:15:59 -0700

    env variable test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const withImages = require('next-images')
 module.exports = withImages({
   env: {
-    SERVER_BASE_URL: process.env.SERVER_BASE_URL || 'http://localhost:8080'
+    SERVER_BASE_URL: 'http://localhost:8080'
   },
 })
