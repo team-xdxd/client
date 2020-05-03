5659ee4dc3c459f25576eada80277cd67360ffa0
PeppermintNDaisies <julianajaimej@gmail.com>
2020-05-02 20:12:32 -0500

    Started with List component for Schedule module

--- components/main/schedule/list/index.tsx
+++ components/main/schedule/list/index.tsx
@@ -1,9 +1,57 @@
+import { useState, useEffect } from 'react'
 import styles from './index.module.css'
-// Components
 
-const List = () => (
-  <section>
-  </section>
-)
+// Components
+import projectApi from '../../../../server-api/project'
+import campaingApi from '../../../../server-api/campaign'
+import taskApi from '../../../../server-api/task'
+
+const List = () => {
+
+  const [projects, setProjects] = useState()
+  const [campaings, setCampaings] = useState()
+  const [tasks, setTasks] = useState()
+
+  useEffect(() => {
+    getProjects()
+    getCampaings()
+    getTasks()
+  }, [])
+
+  const getProjects = async () => {
+    try {
+      const { data } = await projectApi.getProjects()
+      setProjects(data)
+    } catch (err) {
+      console.log(err)
+      // TODO: Error handling
+    }
+  }
+
+  const getCampaings = async () => {
+    try {
+      const { data } = await campaingApi.getCampaigns()
+      setCampaings(data)
+    } catch (err) {
+      console.log(err)
+      // TODO: Error handling
+    }
+  }
+
+  const getTasks = async () => {
+    try {
+      const { data } = await taskApi.getTasks()
+      setTasks(data)
+    } catch (err) {
+      console.log(err)
+      // TODO: Error handling
+    }
+  }
+
+  return (
+    <section>
+    </section>
+  )
+}
 
 export default List
