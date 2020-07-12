35a93ea7da9cad58e999e9782d57ab5247f696a4
PeppermintNDaisies <julianajaimej@gmail.com>
2020-07-12 18:35:19 -0500

    :sparkles: New Feature. Added List View to Asset Library component

--- components/common/asset/list-item.tsx
+++ components/common/asset/list-item.tsx
@@ -15,7 +15,7 @@ const ListItem = ({assetItem, index}) => {
           <div className={styles.header}>
             {index === 0 &&
               <>
-                <h4>Thumbnail</h4>
+                <h4> </h4>
                 <h4>Name</h4>
                 <h4>Stage</h4>
                 <h4>Type</h4>
@@ -29,22 +29,22 @@ const ListItem = ({assetItem, index}) => {
             <div className={styles.thumbnail}>
               <img src={assetItem.thumbailUrl} />
             </div>
-            <div className={styles.name}>
+            <div className={styles.field_name}>
               {assetItem.asset.name}
             </div>
             <div className={styles.status}>
               <StatusBadge status={assetItem.asset.stage} />
             </div>
-            <div className={styles.name}>
+            <div className={styles.field_name}>
               {assetItem.asset.type}
             </div>
-            <div className={styles.name}>
+            <div className={styles.field_name}>
               {assetItem.asset.extension}
             </div>
-            <div className={styles.name}>
+            <div className={styles.field_name}>
               {filesize(assetItem.asset.size)}
             </div>
-            <div className={styles.name}>
+            <div className={styles.field_name}>
               {format(new Date(assetItem.asset.createdAt), dateFormat)}
             </div>
           </div>
