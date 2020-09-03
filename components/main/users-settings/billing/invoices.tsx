1d34e1adf227bac4e72daf933faaaf397d8ec116
Santiago <santibelca@gmail.com>
2020-09-02 23:28:13 -0700

    :sparkles: loading invoice data from stripe

--- components/main/users-settings/billing/invoices.tsx
+++ components/main/users-settings/billing/invoices.tsx
@@ -1,5 +1,6 @@
 import styles from './invoices.module.css'
 import { useState, useEffect } from 'react'
+import planApi from '../../../../server-api/plan'
 
 // Components
 import InvoiceItem from './invoice-item'
@@ -7,16 +8,30 @@ import InvoiceItem from './invoice-item'
 const Invoices = () => {
   const [invoices, setInvoices] = useState([])
   const [upcoming, setUpcoming] = useState([])
+  const [hasMore, setHasMore] = useState(false)
 
   useEffect(() => {
     getInvoices()
+    getUpcoming()
   }, [])
 
   const getInvoices = async () => {
     try {
+      const { data: { hasMore, invoices } } = await planApi.getInvoices()
 
+      setHasMore(hasMore)
+      setInvoices(invoices)
     } catch (err) {
+      console.log(err)
+    }
+  }
 
+  const getUpcoming = async () => {
+    try {
+      const { data } = await planApi.getUpcomingInvoice()
+      setUpcoming([data])
+    } catch (err) {
+      console.log(err)
     }
   }
 
@@ -29,12 +44,21 @@ const Invoices = () => {
     </li>
   )
 
+  console.log(invoices)
+
+  const parsedInvoices = invoices
+    .map(invoice => ({
+      ...invoice,
+      date: (!isNaN(invoice.statusTransitions.paid_at) && new Date(invoice.statusTransitions.paid_at * 1000)) || ''
+    }))
+  const parsedUpcoming = upcoming.map(upcoming => ({ ...upcoming, date: new Date(upcoming.date * 1000) }))
+
   return (
     <div>
       <h3>Invoices</h3>
       <ul>
         <Headers />
-        {invoices.map((invoice, index) => (
+        {parsedInvoices.map((invoice, index) => (
           <li key={index}>
             <InvoiceItem invoice={invoice} />
           </li>
@@ -44,7 +68,7 @@ const Invoices = () => {
       <h3>Upcoming Charges</h3>
       <ul>
         <Headers type='upcoming' />
-        {upcoming.map((invoice, index) => (
+        {parsedUpcoming.map((invoice, index) => (
           <li key={index}>
             <InvoiceItem invoice={invoice} type='upcoming' />
           </li>
--- server-api/plan.js
+++ server-api/plan.js
@@ -3,5 +3,7 @@ import axios from 'axios'
 const planUrl = `${process.env.SERVER_BASE_URL}/plans`
 
 export default {
-  getPlanDetail: () => axios.get(planUrl)
+  getPlanDetail: () => axios.get(planUrl),
+  getInvoices: () => axios.get(`${planUrl}/invoices`),
+  getUpcomingInvoice: () => axios.get(`${planUrl}/upcoming-invoice`)
 }
