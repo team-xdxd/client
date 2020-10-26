03d70c216724b57ae2fa9facb3c6674d74d9ad59
lbeltranc <60752209+lbeltranc@users.noreply.github.com>
2020-10-26 16:35:37 -0500

    added project before task to schedule

--- components/common/misc/type-badge.tsx
+++ components/common/misc/type-badge.tsx
@@ -3,19 +3,25 @@ import styles from './type-badge.module.css'
 import { ProjectTypeChannel, ProjectTypes, ProjectType } from '../../../assets'
 
 
-const StatusBadge = ({ type, socialChannel, name, isMultiple = false }) => {
+const TypeBadge = ({ type, socialChannel, name, isMultiple = false, projectTask}) => {
   let icon = ProjectTypes[type]
   if (type !== 'campaign' && type !== 'task') {
     icon = ProjectType[type]
   }
+
+  let projectName = null
+  if(type === 'task'){
+    projectName = `${projectTask} > `
+  }
+
   return (
     <div className={`${styles[type]} ${styles.container} ${isMultiple && styles.multiple} type-badge`}>
       <img src={socialChannel ? ProjectTypeChannel[socialChannel.toLowerCase()] : icon} />
       <div className={`${styles.name} name`}>
-        {name}
+        <div className={`${styles['project-task']}`}>{projectName}</div>{name}
       </div>
     </div>
   )
 }
 
-export default StatusBadge
+export default TypeBadge
