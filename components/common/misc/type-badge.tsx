d7659a669bdf2af4e8a9c9b4b3ad2bae586c1fe2
lbeltranc <60752209+lbeltranc@users.noreply.github.com>
2020-10-30 15:49:45 -0500

    adde more button to calendat

--- components/common/misc/type-badge.tsx
+++ components/common/misc/type-badge.tsx
@@ -1,6 +1,6 @@
 import { capitalCase } from 'change-case'
 import styles from './type-badge.module.css'
-import { ProjectTypeChannel, ProjectTypes, ProjectType } from '../../../assets'
+import { ProjectTypeChannel, ProjectTypes, ProjectType, Utilities } from '../../../assets'
 
 
 const StatusBadge = ({ type, socialChannel, name, isMultiple = false }) => {
@@ -8,11 +8,16 @@ const StatusBadge = ({ type, socialChannel, name, isMultiple = false }) => {
   if (type !== 'campaign' && type !== 'task') {
     icon = ProjectType[type]
   }
+
+
   return (
     <div className={`${styles[type]} ${styles.container} ${isMultiple && styles.multiple} type-badge`}>
       <img src={socialChannel ? ProjectTypeChannel[socialChannel.toLowerCase()] : icon} />
       <div className={`${styles.name} name`}>
         {name}
+      </div>
+      <div className={`${styles['more-task']}`} onClick={() =>(alert("I am an alert box!"))}>
+        <img src={Utilities.more} />
       </div>
     </div>
   )
