8e4419063e09ecfc76d683c5ce23d6d8fda6a4f1
stevecode21 <suarezsteve21@gmail.com>
2020-11-14 14:02:29 -0500

    Creating handle to duplicate project and sending project data

--- next.config.js
+++ next.config.js
@@ -1,16 +1,28 @@
-const withImages = require('next-images')
+const withImages = require("next-images");
 module.exports = withImages({
   env: {
-    SERVER_BASE_URL: process.env.SERVER_BASE_URL || 'http://localhost:8080',
-    DROPBOX_API_KEY: process.env.DROPBOX_API_KEY || 'gtwo80vc34l8vjd',
-    STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY || 'pk_test_bK1C20PBomU24spmlMeg4AXp',
-    STRIPE_EXPIRE_PRODUCT_NAME: process.env.STRIPE_EXPIRE_PRODUCT_NAME || 'Free',
-    STRIPE_DEFAULT_SIGNUP_PRICE: process.env.STRIPE_DEFAULT_SIGNUP_PRICE || 'price_1HLF2JI2e9K8b8wpF4aheZKo',
-    DEFAULT_TRIAL_PRODUCT: process.env.DEFAULT_TRIAL_PRODUCT || 'prod_Hv5C1USjYMtYBp',
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || '1053631313639-o0m00gdem0cgd3agg6i4o0iop657llkk.apps.googleusercontent.com',
-    GOOGLE_DEVELOPER_KEY: process.env.GOOGLE_DEVELOPER_KEY || 'AIzaSyAqsbbj0ufdPdUO7tQwkvU1gAPn19hTo3s',
-    APPSYNC_GRAPHQL_REALTIMEURL: process.env.APPSYNC_GRAPHQL_REALTIMEURL || 'wss://it7l2l7v25dvhhet4izohabywu.appsync-realtime-api.us-east-1.amazonaws.com/graphql',
-    INCLUDE_PIXEL: process.env.INCLUDE_PIXEL || 'no',
-    INCLUDE_GOOGLE_ANALYTICS: process.env.INCLUDE_GOOGLE_ANALYTICS || 'no'
+    SERVER_BASE_URL: process.env.SERVER_BASE_URL || "http://localhost:8080",
+    // SERVER_BASE_URL: "https://d13c3bdc9413.ngrok.io",
+    DROPBOX_API_KEY: process.env.DROPBOX_API_KEY || "gtwo80vc34l8vjd",
+    STRIPE_PUBLIC_KEY:
+      process.env.STRIPE_PUBLIC_KEY || "pk_test_bK1C20PBomU24spmlMeg4AXp",
+    STRIPE_EXPIRE_PRODUCT_NAME:
+      process.env.STRIPE_EXPIRE_PRODUCT_NAME || "Free",
+    STRIPE_DEFAULT_SIGNUP_PRICE:
+      process.env.STRIPE_DEFAULT_SIGNUP_PRICE ||
+      "price_1HLF2JI2e9K8b8wpF4aheZKo",
+    DEFAULT_TRIAL_PRODUCT:
+      process.env.DEFAULT_TRIAL_PRODUCT || "prod_Hv5C1USjYMtYBp",
+    GOOGLE_CLIENT_ID:
+      process.env.GOOGLE_CLIENT_ID ||
+      "1053631313639-o0m00gdem0cgd3agg6i4o0iop657llkk.apps.googleusercontent.com",
+    GOOGLE_DEVELOPER_KEY:
+      process.env.GOOGLE_DEVELOPER_KEY ||
+      "AIzaSyAqsbbj0ufdPdUO7tQwkvU1gAPn19hTo3s",
+    APPSYNC_GRAPHQL_REALTIMEURL:
+      process.env.APPSYNC_GRAPHQL_REALTIMEURL ||
+      "wss://it7l2l7v25dvhhet4izohabywu.appsync-realtime-api.us-east-1.amazonaws.com/graphql",
+    INCLUDE_PIXEL: process.env.INCLUDE_PIXEL || "no",
+    INCLUDE_GOOGLE_ANALYTICS: process.env.INCLUDE_GOOGLE_ANALYTICS || "no",
   },
-})
+});
--- server-api/project.js
+++ server-api/project.js
@@ -1,20 +1,33 @@
-import axios from 'axios'
-import queryString from 'querystring'
+import axios from "axios";
+import queryString from "querystring";
 
-const projectUrl = `${process.env.SERVER_BASE_URL}/projects`
+const projectUrl = `${process.env.SERVER_BASE_URL}/projects`;
 
 export default {
   getProjectById: (id) => axios.get(`${projectUrl}/${id}`),
-  getProjects: (queryParams) => axios.get(`${projectUrl}?${queryString.stringify(queryParams)}`),
+  getProjects: (queryParams) =>
+    axios.get(`${projectUrl}?${queryString.stringify(queryParams)}`),
   createProject: (data) => axios.post(projectUrl, data),
   updateProject: (id, data) => axios.patch(`${projectUrl}/${id}`, data),
   deleteProject: (id) => axios.delete(`${projectUrl}/${id}`),
-  addtask: (id, { taskData, assignedUser }) => axios.post(`${projectUrl}/${id}/tasks`, { taskData, assignedUser }),
-  removeTask: (id, taskId) => axios.delete(`${projectUrl}/${id}/task/${taskId}`),
+  addtask: (id, { taskData, assignedUser }) =>
+    axios.post(`${projectUrl}/${id}/tasks`, { taskData, assignedUser }),
+  removeTask: (id, taskId) =>
+    axios.delete(`${projectUrl}/${id}/task/${taskId}`),
   addTag: (id, data) => axios.post(`${projectUrl}/${id}/tags`, data),
   removeTag: (id, tagId) => axios.delete(`${projectUrl}/${id}/tags/${tagId}`),
-  associateAssets: (id, { assetIds }, queryParams = {}) => axios.patch(`${projectUrl}/${id}/assets?${queryString.stringify(queryParams)}`, { assetIds }),
+  associateAssets: (id, { assetIds }, queryParams = {}) =>
+    axios.patch(
+      `${projectUrl}/${id}/assets?${queryString.stringify(queryParams)}`,
+      { assetIds }
+    ),
 
-  addCollaborators: (id, data) => axios.post(`${projectUrl}/${id}/collaborators`, data),
-  removeCollaborators: (id, data) => axios.delete(`${projectUrl}/${id}/collaborators`, { data })
-}
+  addCollaborators: (id, data) =>
+    axios.post(`${projectUrl}/${id}/collaborators`, data),
+  removeCollaborators: (id, data) =>
+    axios.delete(`${projectUrl}/${id}/collaborators`, { data }),
+
+  // endpoint for duplicate
+  createDuplicatedProject: (data) =>
+    axios.post(`${projectUrl}/duplicate`, data),
+};
